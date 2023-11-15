bdec13bd01fd93474b216f1eb6151c5f49a91938
“plavelle7” <plavel200@caledonian.ac.uk>
2023-11-15 20:21:42 +0000

    Add Dec2Hex.java file

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,29 +1,38 @@
 import java.util.Scanner;
 
-class Dec2Hex
-{
+class Dec2Hex {
 
-    public static int Arg1;
-
-    public static void main(String args[])
-    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            System.out.println("Error: Please provide a decimal number as an argument.");
+            return;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        try {
+            int arg1 = Integer.parseInt(args[0]);
+            String hexadecimal = convertToHex(arg1);
+            System.out.println("Hexadecimal representation is: " + hexadecimal);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Input must be an integer.");
+        }
+    }
 
+    private static String convertToHex(int num) {
+        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        int rem;
+        StringBuilder hexadecimal = new StringBuilder();
+
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+        while (num != 0) {
+            rem = num % 16;
+            hexadecimal.insert(0, ch[rem]);
+            num = num / 16;
+        }
+
+        return hexadecimal.toString();
     }
 }
+
 
 
